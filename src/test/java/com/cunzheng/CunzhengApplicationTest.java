593a52fdb80132002b5799dc83b3b2cee62d77f8
batulu <baturu2019@163.com>
2019-07-08 22:08:44 +0800

    add contract query feature

--- src/test/java/com/cunzheng/CunzhengApplicationTest.java
+++ src/test/java/com/cunzheng/CunzhengApplicationTest.java
@@ -1,6 +1,8 @@
 package com.cunzheng;
 
+import com.cunzheng.entity.ContractBean;
 import com.cunzheng.entity.UserBean;
+import com.cunzheng.repository.ContractRepository;
 import com.cunzheng.repository.UserRepository;
 import org.junit.Assert;
 import org.junit.Test;
@@ -9,12 +11,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Date;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class CunzhengApplicationTest {
 
 	@Autowired
 	private UserRepository userRepository;
+	@Autowired
+	private ContractRepository contractRepository;
 
 
 	@Test
@@ -29,10 +35,22 @@ public class CunzhengApplicationTest {
 
 		Assert.assertEquals(5,userRepository.findAll().size());
 
-		Assert.assertEquals(4,userRepository.findByUserName("eee").getUserId());
+		Assert.assertEquals(5,userRepository.findByUserName("eee").getUserId());
 
 		userRepository.delete(userRepository.findByUserName("aaa"));
 		Assert.assertEquals(4,userRepository.findAll().size());
+
+		contractRepository.save(new ContractBean(1,"",new Date(),"fangdong","fangke"));
+		contractRepository.save(new ContractBean(2,"",new Date(),"fangdong1","fangke1"));
+		contractRepository.save(new ContractBean(3,"",new Date(),"fangdong2","fangke2"));
+		contractRepository.save(new ContractBean(4,"",new Date(),"fangdong3","fangke3"));
+		contractRepository.save(new ContractBean(5,"",new Date(),"fangdong4","fangke4"));
+
+
+		Assert.assertEquals(5,contractRepository.findAll().size());
+
+
+		System.out.println(contractRepository.findByContractId(1));
 
 	}
 
