e898d27c4b756beb52ab21a0b66a67775e939cab
LifengXiang <xlf_015@163.com>
2019-07-08 20:53:29 +0800

    Update Swagger2.java

--- src/main/java/com/cunzheng/configuration/Swagger2.java
+++ src/main/java/com/cunzheng/configuration/Swagger2.java
@@ -24,7 +24,7 @@ public class Swagger2 {
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.cunzheng_01.controller"))
+                .apis(RequestHandlerSelectors.basePackage("com.cunzheng.controller"))
                 .paths(PathSelectors.any())
                 .build();
     }
