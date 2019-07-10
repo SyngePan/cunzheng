5fde790cfdce150c7ee9bfc7685634088c78dedf
xianglifeng <xianglifeng015@gmail.com>
2019-07-10 14:13:01 +0800

    add Slf4j

--- src/main/java/com/cunzheng/CunzhengApplication.java
+++ src/main/java/com/cunzheng/CunzhengApplication.java
@@ -2,6 +2,7 @@ package com.cunzheng;
 
 import com.cunzheng.entity.ContractBean;
 import com.cunzheng.repository.ContractRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
